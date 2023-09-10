969df0accf2a940c556f9bb129e1c56f7c383803
Kyan Azaizeh <kyanazaizeh@Kyans-MacBook-Pro-2.local>
2023-09-10 13:04:16 +0300

    Added databse with sql code

---
+++ DB/sqlCodes.sql
@@ -0,0 +1,15 @@
+-- Insert a new driver record
+INSERT INTO Drivers (DriverID, UserName, Password, DriversName)
+VALUES (1, 'driver1', 'password123', 'John Doe');
+
+-- Insert a new shipping record associated with DriverID 1
+INSERT INTO ShippingInfo (ShippingID, Date, Time, ShippingContainer, TransportationType, DriverID, VehicleNumber, Port, Status, Company)
+VALUES (1, '2023-09-10', '08:00:00', 'Container123', 'Truck', 1, 'ABC123', 'Port A', 'In Progress', 'ABC Logistics');
+
+-- Delete the driver with DriverID 1
+DELETE FROM Drivers
+WHERE DriverID = 1;
+
+-- Delete the shipping record with ShippingID 1
+DELETE FROM ShippingInfo
+WHERE ShippingID = 1;
