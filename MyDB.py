819ec1702987cee11f24ee0f7d0b9c7fd04dc562
Kyan Azaizeh <kyanazaizeh@Kyans-MacBook-Pro-2.local>
2023-09-15 12:50:29 +0300

    Created and added queries for database management

--- MyDB.py
+++ MyDB.py
@@ -1,65 +1,126 @@
 import mysql.connector
 
 # Connection parameters
-host = "localhost"  # Replace with your MySQL server's hostname or IP address
+host = "localhost"
 user = "root"
 password = "Kyanaz25"
 database = "WebDB"
 
 # Create a connection
-try:
-    connection = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
-    )
-
-    if connection.is_connected():
-        print("Connected to MySQL server")
-
-        # Create a cursor
-        cursor = connection.cursor()
-
-        # Drop the Users table if it exists
-        '''
-        cursor.execute("DROP TABLE IF EXISTS Users")
-        '''
-        
-        # Define the CREATE TABLE statement with an auto-incrementing UserID
-        '''
-        create_table_query = """
-        CREATE TABLE Users (
-            UserID INT AUTO_INCREMENT PRIMARY KEY,
-            Name VARCHAR(25) NOT NULL,
-            Password VARCHAR(25) NOT NULL
+def create_connection():
+    try:
+        connection = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
         )
-        """
 
-        # Execute the CREATE TABLE statement
-        cursor.execute(create_table_query)
-        print("Users table created successfully.")
-        '''
+        if connection.is_connected():
+            print("Connected to MySQL server")
+            return connection
 
-        # Insert a new user into the Users table (UserID will be generated automatically)
-        insert_query = "INSERT INTO Users (Name, Password) VALUES (%s, %s)"
-        user_data = ("Kyan", "123")  # Replace with your user data
-        cursor.execute(insert_query, user_data)
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return None
+#_______________________________________________________________________
 
-        # Commit the transaction (save the changes to the database)
+# Define a function to delete a user by name
+def delete_user_by_name(connection, cursor, name):
+    try:
+        delete_query_name = "DELETE FROM Users WHERE Name = %s"
+        cursor.execute(delete_query_name, (name,))
         connection.commit()
+        print("Changes saved successfully")
 
-        # Select and print all users in the Users table
-        cursor.execute("SELECT * FROM Users")
-        for user in cursor.fetchall():
-            print(f"UserID: {user[0]}, Name: {user[1]}, Password: {user[2]}")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+#_______________________________________________________________________
 
-except Exception as e:
-    print(f"Error: {str(e)}")
+# Define a function to delete a user by ID
+def delete_user_by_id(connection, cursor, id):
+    try:
+        delete_query_id = "DELETE FROM Users WHERE ID = %s"
+        cursor.execute(delete_query_id, (id,))
+        connection.commit()
+        print("Changes saved successfully")
 
-finally:
+    except Exception as e:
+        print(f"Error: {str(e)}")
+#_______________________________________________________________________
+
+# Define a function to print the contents of a table
+def print_table(connection, cursor, table_name):
+    try:
+        cursor.execute(f"SELECT * FROM {table_name}")
+
+        # Get column names from the cursor's description
+        column_names = [desc[0] for desc in cursor.description]
+
+        # Print the column names (header)
+        print("Table Header:")
+        for column_name in column_names:
+            print(column_name, end=" ")
+        print()
+
+        # Fetch and print results
+        for row in cursor.fetchall():
+            print(row)
+
+    except Exception as e:
+        print(f"Error: {str(e)}")
+#_______________________________________________________________________
+
+# Define a function to add a new user
+def add_user(connection, cursor, name, email):
+    try:
+        # Insert a new user into the Users table
+        insert_query = "INSERT INTO Users (Name, Email) VALUES (%s, %s)"
+        user_data = (name, email)
+        cursor.execute(insert_query, user_data)
+        connection.commit()
+        print(f"User {name} added successfully")
+
+    except Exception as e:
+        print(f"Error: {str(e)}")
+#_______________________________________________________________________
+
+# Define a function to authenticate a user
+def authenticate_user(connection, cursor, username, password):
+    try:
+        # Query the database for the user's credentials
+        select_query = "SELECT * FROM Users WHERE Name = %s AND Password = %s"
+        user_data = (username, password)
+        cursor.execute(select_query, user_data)
+
+        # Fetch the user data (if found)
+        user = cursor.fetchone()
+
+        if user:
+            print(f"Authentication successful for user: {user[1]}")
+            return True
+        else:
+            print("Authentication failed. Invalid username or password.")
+            return False
+
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return False
+#_______________________________________________________________________
+
+# Create a connection and cursor
+connection = create_connection()
+if connection:
+    cursor = connection.cursor()
+
+    # Example usage of functions
+    delete_user_by_name(connection, cursor, 'John Doe')
+    delete_user_by_id(connection, cursor, 123)
+    print_table(connection, cursor, 'Users')
+
     # Close the cursor and connection
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("Connection closed")
+    cursor.close()
+    connection.close()
+    print("Connection closed")
+#_______________________________________________________________________
+
